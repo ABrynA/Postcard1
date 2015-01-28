b3677deadedd268fa8d11c50b87fa66304203c54
Anselm <anselm@A-MacBook-Pro-2.local>
2015-01-28 06:48:33 -0400

    Initial Commit

---
+++ Post/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Post
+//
+//  Created by Anselm on 1/28/15.
+//  Copyright (c) 2015 Anselm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
